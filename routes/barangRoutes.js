8c86f3ab01aeca3853f058c3f287f00cb8006184
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2024-11-09 10:07:20 +0700

    style: delete

--- routes/barangRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const barangController = require('../controllers/barangController');
-const auth = require('../middleware/auth');
-
-router.post('/barang', auth, barangController.createBarang);
-
-module.exports = router;
