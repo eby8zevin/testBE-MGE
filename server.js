163c4e7d33f9dd73c205c835125d389f517665f5
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2024-11-09 10:05:50 +0700

    refactor: re-config

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const userRoutes = require('./routes/userRoutes');
-const barangRoutes = require('./routes/barangRoutes');
 
 dotenv.config();
+
 const app = express();
 app.use(express.json());
 
 app.use('/api', userRoutes);
-app.use('/api', barangRoutes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
